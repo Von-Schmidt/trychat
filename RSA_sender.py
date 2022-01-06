8c4f119eebe840aed7d44215bcb5abc38f3d6b2d
Vincent Šmíd <45830825+Von-Schmidt@users.noreply.github.com>
2022-01-06 10:22:21 +0100

    Update RSA_sender.py
    added checking for duplicates

--- RSA_sender.py
+++ RSA_sender.py
@@ -4,11 +4,12 @@ import random
 def sending(plaintext):
     primelist = RSA_lib.primesInRange(1000, 2000) # make a list of primes in range
     p = random.choice(primelist) # choose a prime from the list
-    q = random.choice(primelist) # choose a second one - to implement checking for duplicates
-    publickey, privatekey = RSA_lib.keygen(p, q)
+    q = random.choice(primelist) # choose a second one
+    while p == q: # in case the same prime gets chosen for both variables
+        q = random.choice(primelist)
+    publickey, privatekey = RSA_lib.keygen(p, q) # generate the public and private keys
     message = plaintext
-    encrypted_msg = RSA_lib.encrypt(privatekey, message)
-    package = publickey, encrypted_msg
+    encrypted_msg = RSA_lib.encrypt(privatekey, message) # encrypt the message with private key
     return publickey, encrypted_msg
 
 # replace this with a text input
