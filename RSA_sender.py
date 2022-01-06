519d3ac510d3f45e4529e94304c99b3f04530783
Vincent Šmíd <45830825+Von-Schmidt@users.noreply.github.com>
2022-01-06 09:48:39 +0100

    Update RSA
    Made it easy to input

--- RSA_sender.py
+++ RSA_sender.py
@@ -1,17 +1,17 @@
 import RSA_lib
 import random
 
-def sending():
-    primelist = RSA_lib.primesInRange(1000, 2000)
-    p = random.choice(primelist)
-    q = random.choice(primelist)
+def sending(plaintext):
+    primelist = RSA_lib.primesInRange(1000, 2000) # make a list of primes in range
+    p = random.choice(primelist) # choose a prime from the list
+    q = random.choice(primelist) # choose a second one - to implement checking for duplicates
     publickey, privatekey = RSA_lib.keygen(p, q)
-    message = "zdenda"
+    message = plaintext
     encrypted_msg = RSA_lib.encrypt(privatekey, message)
     package = publickey, encrypted_msg
     return publickey, encrypted_msg
 
 # replace this with a text input
-package = sending()
+package = sending("zdeněk")
 
 print(package) # output is in format publickey, encrypted_msg
